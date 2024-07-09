ef82de70cca4caa92ef716296bcb6a3d7473bd51
ayou <550244300@qq.com>
2024-07-09 23:33:29 +0800

    feat:升级版本

--- src/main/java/com/ayou/ai/ChatAPI.java
+++ src/main/java/com/ayou/ai/ChatAPI.java
@@ -2,10 +2,10 @@ package com.ayou.ai;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,23 +19,21 @@ import java.util.Map;
 @RestController
 @RequestMapping("ai")
 public class ChatAPI {
-    private final OllamaChatClient chatClient;
+    private final OllamaChatModel chatModel;
 
     @Autowired
-    public ChatAPI(OllamaChatClient chatClient) {
-        this.chatClient = chatClient;
+    public ChatAPI(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
     }
 
-    @GetMapping("generate")
-    public Map generate(@RequestParam(value = "msg", defaultValue = "Tell me a joke") String message) {
-        String call = chatClient.call(message);
-        log.info(call);
-        return Map.of("generation", call);
+    @GetMapping("/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", chatModel.call(message));
     }
 
-    @GetMapping("generateStream")
+    @GetMapping("/generateStream")
     public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         Prompt prompt = new Prompt(new UserMessage(message));
-        return chatClient.stream(prompt);
+        return chatModel.stream(prompt);
     }
 }
